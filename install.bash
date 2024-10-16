c3a9683dd8ce06f79f8c6346e914b9833efe3459
Athira Krishnan R <athirakrishnanr94@gmail.com>
2024-10-16 10:50:21 +0530

    Update install.bash

--- install.bash
+++ install.bash
@@ -5,7 +5,7 @@ pip install -r avlmaps/audioclip/requirements.txt
 conda install habitat-sim=0.2.2 -c conda-forge -c aihabitat -y
 
 pip install -e .
-
+pip install git+https://github.com/openai/CLIP.git
 cd ~
 git clone --recursive https://github.com/cvg/Hierarchical-Localization/
 cd Hierarchical-Localization/
